124c9abeec4cddd0802fff787c61b9bff6a221f3
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2022-12-13 11:46:15 +0100

    -

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+let slidesContainer = document.getElementById('slides-container');
+let slide = document.querySelector('.slide');
+let prevButton = document.getElementById('slide-arrow-prev');
+let nextButton = document.getElementById('slide-arrow-next');
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
+nextButton.addEventListener('click', () => {
+    let slideWidth = slide.clientWidth;
+
   slidesContainer.scrollLeft += slideWidth;
 });
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
+prevButton.addEventListener('click', () => {
+    let slideWidth = slide.clientWidth;
+
   slidesContainer.scrollLeft -= slideWidth;
 });
